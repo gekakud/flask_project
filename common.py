6d506b6c2dcc01794c0bd177e14e19e48c50b082
Evgeny Pekerman <Evgeny.Pekerman@dspg.com>
2021-01-11 14:54:42 +0200

    typo

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 # common strings
 main_view_page_title = "Main view"
 not_found_error_message = "ERROR 404 - resource not found!"
-internal_server_error = "ERROE 500 - see logs!"
+internal_server_error = "ERROR 500 - see logs!"
 
 # configs
 mongodb_connection_string = "mongodb+srv://AdminUser:geka2009@cluster0.yrqjt.mongodb.net/demo_configs_db" \
