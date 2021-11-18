34a47b9c1697d4c190220bcc75ae43cdce001880
evgeny.pekerman <evgeny.pekerman@dspg.com>
2021-11-18 11:22:07 +0200

    Update common.py

--- common.py
+++ common.py
@@ -4,7 +4,7 @@ not_found_error_message = "ERROR 404 - resource not found!"
 internal_server_error = "ERROR 500 - see logs!"
 
 # configs
-mongodb_connection_string = "mongodb+srv://AdminUser:geka2009@cluster0.yrqjt.mongodb.net/demo_configs_db" \
+mongodb_connection_string = "mongodb+srv://AdminUser:geka2009@cluster0.yrqjt.mongodb.net/configs_db" \
                             "?retryWrites=true&w=majority"
 
 # gitlab
